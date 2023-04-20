0868ec277c8a3c448e39996a821f343e6690826a
Romanhessec <romangabimarian@gmail.com>
2023-04-20 20:46:37 +0300

    All done except logging + unit testing

--- skel/tema/consumer.py
+++ skel/tema/consumer.py
@@ -1,42 +1,26 @@
-"""
-This module represents the Consumer.
-
-Computer Systems Architecture Course
-Assignment 1
-March 2021
-"""
-
 from threading import Thread
 import time
 
-
 class Consumer(Thread):
-    """
-    Class that represents a consumer.
-    """
-
     def __init__(self, carts, marketplace, retry_wait_time, **kwargs):
-        """
-        Constructor.
-
-        :type carts: List
-        :param carts: a list of add and remove operations
-
-        :type marketplace: Marketplace
-        :param marketplace: a reference to the marketplace
-
-        :type retry_wait_time: Time
-        :param retry_wait_time: the number of seconds that a producer must wait
-        until the Marketplace becomes available
-
-        :type kwargs:
-        :param kwargs: other arguments that are passed to the Thread's __init__()
-        """
-
         Thread.__init__(self, **kwargs)
         self.carts = carts
         self.marketplace = marketplace
         self.retry_wait_time = retry_wait_time
+        self.name = kwargs['name']
 
     def run(self):
-        pass
+        for cart in self.carts:
+            cart_id = self.marketplace.new_cart()
+
+            for request in cart:
+                if request["type"] == "add":
+                    for _ in range(request["quantity"]):
+                        while not self.marketplace.add_to_cart(cart_id, request["product"]):
+                            time.sleep(self.retry_wait_time)
+                elif request["type"] == "remove":
+                    for _ in range(request["quantity"]):
+                        self.marketplace.remove_from_cart(cart_id, request["product"])
+
+            self.marketplace.place_order(cart_id)
+
